49179fa0d0ba3f13002adb5284771f4d0c8fbd91
Woocheck <34909593+Woocheck@users.noreply.github.com>
2019-04-25 21:51:08 +0200

    Test for the two-wheel drive class.

--- main.cpp
+++ main.cpp
@@ -6,43 +6,42 @@
 */
 
 
-//#include "./pin_settings.h"
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include "./pin_settings.h"
 #include <unistd.h>
 #include <wiringPi.h>
 #include <softPwm.h>
+#include "./DC_motor_class.h"
 #include "./Two_wheel_drive_class.h"
+#include <vector>
 
-TwoWheelDriveClass drive;
+
 
 int main(void)
 {
-    wiringPiSetupPhys();
-    //drive.goFoward();
-    //drive.setSpeed(120);
-    while (1) 
+    wiringPiSetup ()  ;
+    char buf [80] ;
+  
+    TwoWheelDriveClass drive;
+    while(1)
     {
-	    /*
-		
-		usleep(1000000);
-		drive.stop();
-		usleep(1000000);
-		drive.goFoward();
-		usleep(1000000);
-		drive.turnLeft();
-		usleep(1000000);
-		drive.turnRight();
-		usleep(1000000);
-		drive.goBackward();
-		* */
-	pinMode (22, OUTPUT);
-	pinMode (23, OUTPUT) ;
-	softPwmCreate(24, 0, 100);
-	
-	digitalWrite (23, LOW); 
-	digitalWrite (22, HIGH);
-	softPwmCreate(24, 50, 100);
-	while(1);
-		
+	drive.setSpeed(100);
+	fgets (buf, 80, stdin) ;
+	drive.goForward();
+	fgets (buf, 80, stdin) ;
+	drive.goBackward();
+	fgets (buf, 80, stdin) ;
+	drive.setSpeed(10);
+	fgets (buf, 80, stdin) ;
+	drive.stop();
+	fgets (buf, 80, stdin) ;
+	drive.goForward();
+	fgets (buf, 80, stdin) ;
+	drive.turnLeft();
+	fgets (buf, 80, stdin) ;
+	drive.turnRight();
+	fgets (buf, 80, stdin) ;
     }
 }
-
