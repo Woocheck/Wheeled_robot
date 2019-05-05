964fbab53135a4fe44f437d4cdcfbb148f1fd465
Woocheck <34909593+Woocheck@users.noreply.github.com>
2019-05-05 22:13:02 +0200

    Profilers Class created in first version.

--- profiler/profilerClass.cpp
+++ profiler/profilerClass.cpp
@@ -1,1 +1,63 @@
+/* 
+* profilerClass.h.h
+*
+* Created: 01.05.2019
+* Author: Woocheck
+*/
+#include <cstdlib>
+#include "profilerClass.h"
 
+int Profiler::getCalculatedRotation()
+{
+   return calculateTheRotation();
+};
+void Profiler::setGivenAcceleration(const int acceleration)
+{
+   givenAcceleration = acceleration;
+};
+int getGivenAcceleration();
+
+void Profiler::setTargetSpeed(const int speed)
+{
+   targetSpeed = speed;
+};
+int Profiler::getTargetSpeed()
+{
+   return targetSpeed;
+};
+int Profiler::calculateTheRotation()
+{
+   if(status == ProfilerStatus::drive) 
+ { 
+     if(currentSpeed * ((currentSpeed + 128) >> 8)/(2 * givenAcceleration) >= std::abs(target_T) - std::abs(zad_T)) 
+     { 
+         status = ProfilerStatus::braking; 
+         docelowa_T_8 = nextStepTargetSpeed; 
+     } 
+ } 
+ 
+ if(status == ProfilerStatus::braking && currentSpeed == 0) 
+ { 
+     status = ProfilerStatus::end; 
+     docelowa_T_8 = nextStepTargetSpeed; 
+ } 
+ 
+ if(currentSpeed < targetSpeed) 
+ { 
+     currentSpeed += givenAcceleration; 
+     if(currentSpeed > targetSpeed) 
+         currentSpeed = targetSpeed; 
+ } 
+ 
+ if(currentSpeed > targetSpeed) 
+ { 
+     currentSpeed -= givenAcceleration; 
+     if(currentSpeed < targetSpeed) 
+         currentSpeed = targetSpeed; 
+ } 
+ 
+ if(target_T > 0) 
+     calculatedRotation += ((currentSpeed + 128) >> 8); 
+ else 
+     calculatedRotation -= ((currentSpeed + 128) >> 8);
+};
--- profiler/profilerClass.h
+++ profiler/profilerClass.h
@@ -1,1 +1,43 @@
+/* 
+* profilerClass.h.h
+*
+* Created: 01.05.2019
+* Author: Woocheck
+*/
 
+#ifndef __PROFILER_CLASS_H__
+#define __PROFILER_CLASS_H__
+
+
+enum class ProfilerStatus {drive, braking, end};
+
+class Profiler 
+{
+   private:
+   ProfilerStatus status {ProfilerStatus::drive};
+   int targetSpeed  {50};        // docelowa_T - docelowa prędkość, do której stopniowo dąży profiler 
+                                 // max_T - maksymalna dozwolona prędkość 
+   int nextStepTargetSpeed {50};
+   int currentSpeed {0};         // aktualnie zadana prędkość 
+   int givenAcceleration {10};                // zadane przyspieszenie 
+   int calculatedRotation {0};           // wyliczony kąt do przejechania - wykorzystywany jako setpoint regulatora PD 
+   int targetAngle {45};                // zadany całkowity kąt do przejechania
+   
+   public:
+   Profiler() = default;
+   Profiler(int max): targetSpeed{max}{};
+   
+   int getCalculatedRotation();
+
+   void setGivenAcceleration(int);
+   int getGivenAcceleration();
+   
+   void setTargetSpeed(int);
+   int getTargetSpeed();
+
+   private:
+   int calculateTheRotation();
+   int Profiler::calculateTheTranslation();
+};   
+
+#endif
