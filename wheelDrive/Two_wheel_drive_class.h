ea77318b356a28cf60b0c2e15a78957ac0441698
Wojciech Luczkowski <wojciech.luczkowski@op.pl>
2019-05-10 21:01:03 +0200

    Encoders and regulators PD added to TwoWheelDriveClass

--- wheelDrive/Two_wheel_drive_class.h
+++ wheelDrive/Two_wheel_drive_class.h
@@ -13,7 +13,7 @@
 #include </home/user/workspace_project/wiringPi/wiringPi/softPwm.h>
 #include "/home/user/workspace_project/Wheeled_robot/pin_settings.h"
 #include "/home/user/workspace_project/Wheeled_robot/dcMotor/DC_motor_class.h"
-
+#include "/home/user/workspace_project/Wheeled_robot/regulatorPD/regulatorPDClass.h"
 
 
 class TwoWheelDriveClass
@@ -32,16 +32,35 @@ double angleToGo {0};
 double totalAngleFromEnkoder {0};                
    
 int minimalPWM {0}, maximalPWM {100};
+double translationProportionalParameter {1}, translationDerivateParameter {1};
+double rotationProportionalParameter {1}, rotationDerivateParameter {1};
 
 DcMotorClass Left_DC;
 DcMotorClass Right_DC;
+
+Encoder encoderLeft;
+Encoder encoderRight;
+
+RegulatorPD translationRegulator;
+RegulatorPD rotationRegulator;
 
 int speed {0};
 Direction direction {Direction::forward};
 
 //functions
 public:
-	TwoWheelDriveClass():Left_DC(28,29,25),Right_DC(22,23,24){
+	TwoWheelDriveClass():
+	Left_DC( 28, 29, 25 ),
+	Right_DC( 22, 23, 24 ),
+	encoderLeft( PIN_ENCODER_LEFT_A, PIN_ENCODER_LEFT_B ),
+	encoderRight( PIN_ENCODER_RIGHT_A, PIN_ENCODER_RIGHT_B ),
+	translationRegulator( maximalPWM, minimalPWM, 
+								translationProportionalParameter, 
+								translationDerivateParameter ),
+	rotationRegulator( maximalPWM, minimalPWM, 
+								translationProportionalParameter, 
+								translationDerivateParameter )
+	{
 		wiringPiSetup();
 	};
 	~TwoWheelDriveClass();
