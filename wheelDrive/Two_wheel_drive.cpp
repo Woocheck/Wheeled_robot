41289381eb34f0283a7bdbbf207794728c1a2c35
Woocheck <woocheck@op.pl>
2019-05-14 22:50:33 +0200

    Added function to cooperate with encoders interrupts

---
+++ wheelDrive/Two_wheel_drive.cpp
@@ -0,0 +1,71 @@
+/* 
+* Two_wheel_drive_class.cpp
+*
+* Created: 15.03.2019 18:50:45
+* Author: Woocheck
+*/
+
+#include </home/user/workspace_project/wiringPi/wiringPi/wiringPi.h>
+#include </home/user/workspace_project/wiringPi/wiringPi/softPwm.h>
+#include "/home/user/workspace_project/Wheeled_robot/pin_settings.h"
+
+#include "/home/user/workspace_project/Wheeled_robot/dcMotor/DC_motor_class.h"
+#include "/home/user/workspace_project/Wheeled_robot/wheelDrive/Two_wheel_drive.h"
+
+#include "/home/user/workspace_project/Wheeled_robot/regulatorPD/regulatorPD.h"
+#include "/home/user/workspace_project/Wheeled_robot/encoder/encoder.h"
+
+
+TwoWheelDrive::~TwoWheelDrive()
+{
+} 
+
+void TwoWheelDrive::setSpeed(int demandedSpeed)
+{
+	speed=demandedSpeed;
+	Left_DC.control(direction, speed);
+	Right_DC.control(direction, speed);
+};
+void TwoWheelDrive::goForward()
+{
+	direction = Direction::forward;
+	Left_DC.control(Direction::forward, speed);
+	Right_DC.control(Direction::forward, speed);
+};
+void TwoWheelDrive::goBackward()
+{
+	direction = Direction::backward;
+	Left_DC.control(Direction::backward, speed);
+	Right_DC.control(Direction::backward, speed);
+};
+void TwoWheelDrive::turnLeft()
+{
+	direction = Direction::left;
+	Left_DC.stop();
+	Right_DC.control(Direction::forward, speed);
+};
+void TwoWheelDrive::turnRight()
+{
+	direction = Direction::right;
+	Left_DC.control(Direction::forward, speed);
+	Right_DC.stop();
+};
+
+void TwoWheelDrive::stop()
+{
+	Left_DC.stop();
+	Right_DC.stop();
+};
+
+void TwoWheelDrive::setNewTask(int newTranslation, 
+												int newRotation )
+{
+	distanceToGo = newTranslation;
+	angleToGo = newRotation;
+}
+
+void TwoWheelDrive::readEncoders()
+{
+	encoderLeft.readDistance();
+	encoderRight.readDistance();
+}
---
+++ wheelDrive/Two_wheel_drive.h
@@ -0,0 +1,78 @@
+/* 
+* Two_wheel_drive_class.h
+*
+* Created: 15.03.2019 18:50:45
+* Author: Woocheck
+*/
+
+
+#ifndef __TWO_WHEEL_DRIVE_CLASS_H__
+#define __TWO_WHEEL_DRIVE_CLASS_H__
+
+#include </home/user/workspace_project/wiringPi/wiringPi/wiringPi.h>
+#include </home/user/workspace_project/wiringPi/wiringPi/softPwm.h>
+#include "/home/user/workspace_project/Wheeled_robot/pin_settings.h"
+#include "/home/user/workspace_project/Wheeled_robot/dcMotor/DC_motor_class.h"
+#include "/home/user/workspace_project/Wheeled_robot/regulatorPD/regulatorPD.h"
+#include "/home/user/workspace_project/Wheeled_robot/encoder/encoder.h"
+
+class TwoWheelDrive
+{
+	private:
+
+		double currentTranslationError {0}; 
+		double distanceToGo {0}; 
+		double totalDistanceFromEnkoder {0};  
+
+		double currentRotationError {0}; 
+		double angleToGo {0}; 
+		double totalAngleFromEnkoder {0};                
+
+		int minimalPWM {0}, maximalPWM {100};
+		double translationProportionalParameter {1}, translationDerivateParameter {1};
+		double rotationProportionalParameter {1}, rotationDerivateParameter {1};
+
+		DcMotorClass Left_DC;
+		DcMotorClass Right_DC;
+
+		Encoder encoderLeft;
+		Encoder encoderRight;
+
+		RegulatorPD translationRegulator;
+		RegulatorPD rotationRegulator;
+
+		int speed {0};
+		Direction direction {Direction::forward};
+
+	public:
+		TwoWheelDrive():
+			Left_DC(  PIN_LEFT_A, PIN_LEFT_B, PIN_LEFT_ENABLE ),
+			Right_DC( PIN_RIGHT_A, PIN_RIGHT_B , PIN_RIGHT_ENABLE ),
+			encoderLeft( PIN_ENCODER_LEFT_A, PIN_ENCODER_LEFT_B ),
+			encoderRight( PIN_ENCODER_RIGHT_A, PIN_ENCODER_RIGHT_B ),
+			translationRegulator( maximalPWM, minimalPWM, 
+										translationProportionalParameter, 
+										translationDerivateParameter ),
+			rotationRegulator( maximalPWM, minimalPWM, 
+										rotationProportionalParameter, 
+										rotationDerivateParameter )
+		{
+			wiringPiSetup();
+		};
+		~TwoWheelDrive();
+		void setSpeed(int demandedSpeed);
+		void goForward();
+		void goBackward();
+		void turnLeft();
+		void turnRight();
+		void stop();
+		void setNewTask(int newTranslation, int newRotation);
+		void readEncoders();
+	
+	private:
+		TwoWheelDrive( const TwoWheelDrive &c );
+		TwoWheelDrive& operator=( const TwoWheelDrive &c );
+
+}; 
+
+#endif 
