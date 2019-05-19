3abe7e2e64a86aa9effc0eab01b55fefd3314386
Woocheck <34909593+Woocheck@users.noreply.github.com>
2019-05-19 09:14:11 +0200

    Corected and tested pins numbers.

--- pin_settings.h
+++ pin_settings.h
@@ -10,18 +10,18 @@
 
 
  
- #define PIN_LEFT_A 13
- #define PIN_LEFT_B 6
- #define PIN_LEFT_ENABLE 26
+ #define PIN_LEFT_A 2
+ #define PIN_LEFT_B 3
+ #define PIN_LEFT_ENABLE 21
  
- #define PIN_RIGHT_A 22
- #define PIN_RIGHT_B 27
- #define PIN_RIGHT_ENABLE 5
+ #define PIN_RIGHT_A 23
+ #define PIN_RIGHT_B 22
+ #define PIN_RIGHT_ENABLE 25
 
- #define PIN_ENCODER_LEFT_A 17
- #define PIN_ENCODER_LEFT_B 4
+ #define PIN_ENCODER_LEFT_A 0
+ #define PIN_ENCODER_LEFT_B 7
 
- #define PIN_ENCODER_RIGHT_A 18
- #define PIN_ENCODER_RIGHT_B 23
+ #define PIN_ENCODER_RIGHT_A 1
+ #define PIN_ENCODER_RIGHT_B 4
  
 #endif /* PIN_SETTINGS_H_ */
