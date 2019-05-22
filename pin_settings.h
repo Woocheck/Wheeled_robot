440e9c13908bbcb1b8c00fdfdcffbed0adaba975
Woocheck <woocheck@op.pl>
2019-05-22 20:25:58 +0200

    Sensors pins number implementation.

--- pin_settings.h
+++ pin_settings.h
@@ -23,5 +23,11 @@
 
  #define PIN_ENCODER_RIGHT_A 1
  #define PIN_ENCODER_RIGHT_B 4
+
+ #define PIN_SENSOR_1 6 
+ #define PIN_SENSOR_2 26
+ #define PIN_SENSOR_3 27
+ #define PIN_SENSOR_4 28
+ #define PIN_SENSOR_5 29
  
 #endif /* PIN_SETTINGS_H_ */
