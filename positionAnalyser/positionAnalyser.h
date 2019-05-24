75368f1276dc783de9f9768dc54c0386a4e8be1d
Woocheck <woocheck@op.pl>
2019-05-24 22:09:35 +0200

    PositionAnalyser Class.

---
+++ positionAnalyser/positionAnalyser.h
@@ -0,0 +1,25 @@
+#ifndef __POSITION_ANALYSER_H__
+#define __POSITION_ANALYSER_H__
+
+#include <vector>
+class PositionAnalyser
+{
+private:
+   std::vector<int> sensorsState { 0, 0, 0, 0, 0 };
+   double calculatedError {0};
+   int previousError {0};
+   int offTheRoad {0};
+   int error {0};
+   int pop_blad = 0, Kp = 1, Kd = 0;
+
+public:
+   PositionAnalyser();
+   ~PositionAnalyser();
+   void setSensorsState(std::vector<int>);
+   void calculateError();
+   double getCalculatedError();
+};
+
+
+
+#endif 
