4b429e7f246aebda31434bc827da7cb78668744f
Woocheck <woocheck@op.pl>
2019-07-03 19:47:46 +0200

    Changed files to compile.

--- recompile.sh
+++ recompile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ main.cpp ./wheelDrive/twoWheelDrive.cpp ./dcMotor/DCmotor.cpp ./encoder/encoder.cpp ./profiler/translationProfiler.cpp ./profiler/rotationProfiler.cpp ./regulatorPD/regulatorPD.cpp positionAnalyser/positionAnalyser.cpp lineDetector/detector.cpp -o main -std=c++17 -lwiringPi -lpthread
+g++ main.cpp ./wheelDrive/twoWheelDrive.cpp ./dcMotor/DCmotor.cpp ./encoder/encoder.cpp ./regulatorPD/regulatorPD.cpp positionAnalyser/positionAnalyser.cpp lineDetector/detector.cpp -o main -std=c++17 -lwiringPi -lpthread
