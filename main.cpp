dae04fd2fcc1e678578cf2a9d98a7c9c1ed94172
Woocheck <woocheck@op.pl>
2019-10-03 22:10:50 +0200

    clear main

--- main.cpp
+++ main.cpp
@@ -23,31 +23,16 @@
 #include "./dcMotor/DCmotor.h"
 #include "./wheelDrive/twoWheelDrive.h"
 #include "./encoder/encoder.h"
-#include "./lineDetector/detector.h"
-#include "./controllerLineFollower/controllerLineFollower.h"
-#include "./controllerLookingForLine/controllerLookingForLine.h"
 #include "./timer/timer.h"
-#include "./putVehicleOnLine/putVehicleOnLine.h"
 #include "./RoadControler/RoadControler.h"
 
-
-
-void regulationInLineFollowerMode();
-void lookingForLineMode();
 bool isPassed20ms();
 bool isOn();
 void readLeftEncoderChange();
 void readRightEncoderChange();
-void readDetectorChange();
 
-bool isLineDetected();
 
 Gpio gpio;
-
-Detector frontLineDetector(  gpio.frontSensor_1, gpio.frontSensor_2, gpio.frontSensor_3,
-                       gpio.frontSensor_4, gpio.frontSensor_5 );
-Detector rearLineDetector( gpio.rearSensor_1, gpio.rearSensor_2, gpio.rearSensor_3,
-                       gpio.rearSensor_4, gpio.rearSensor_5  );
 
 Encoder leftEncoder( gpio.encoderLeftA, gpio.encoderLeftB );
 Encoder rightEencoder( gpio.encoderRightA, gpio.encoderRightB );
@@ -78,46 +63,7 @@ int main( void )
 
   int nominalSpeed { 60 };
   
-  if( isOn()) 
-  {
-    ControllerLookingForLine lineSeeker( drive, frontLineDetector );
-    lineSeeker.startLooking( nominalSpeed );
-    
-    while( !lineSeeker.isFoundTheLine() )
-    {
-      lineSeeker.verifiesMovementCorrectness();
-    }
-    
-    lineSeeker.stopVechicle();
-  }
-
-  if( isOn() )
-  {
-    PutVehicleOnLine putOnLineController( frontLineDetector, rearLineDetector, roadControler );
-    if( !putOnLineController.isVehicleOnLine() )
-      putOnLineController.setOptimalPositionOnLine();
-  }
-
-  if( isOn() )
-  {
-    ControllerLineFollower lineFollowerControl( drive );
-    lineFollowerControl.setSpeed( nominalSpeed );
-
-    while(1)
-    {
-      lineFollowerControl.setSpeed( nominalSpeed );
-
-      if( isOn() && isPassed20ms() )
-      {
-       	frontLineDetector.readSensorsState();
-	      lineFollowerControl.setSensorsState( frontLineDetector.getSensorsState() );
-        lineFollowerControl.calculateError();
-
-	      int correction = static_cast<int>( lineFollowerControl.getCalculatedError() );
-        lineFollowerControl.setDirection( correction );	
-      }
-    }
-  }
+  
 }
 
 bool isPassed20ms()
@@ -142,10 +88,6 @@ void readLeftEncoderChange()
   leftEncoder.readDistance();
 };
 
-void readDetectorChange()
-{
-  frontLineDetector.readSensorsState();
-  rearLineDetector.readSensorsState();
-};
+
 
 
